9594e3bbb4509454c3918de440268e2fba9c530f
DBProductions <dennis@dbproductions.de>
2012-09-29 16:29:41 +0200

    add qunit

---
+++ qunit/tests/allTests.js
@@ -0,0 +1,13 @@
+module("Module A");
+
+test("first test within module", function() {
+    ok( true, "all pass" );
+});
+
+test("second test within module", function() {
+    ok( true, "all pass" );
+});
+
+test("third test within module", function() {
+    ok( true, "all pass" );
+});
