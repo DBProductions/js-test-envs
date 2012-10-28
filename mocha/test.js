2dffc7ee52d2d999aad94df6740f5002e2c18894
DBProductions <dennis@dbproductions.de>
2012-10-28 17:52:26 +0100

    add mocha example

---
+++ mocha/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('do something', function() {
+            assert.equal(-1, [1, 2, 3].indexOf(5));
+        });
+    });
+});
