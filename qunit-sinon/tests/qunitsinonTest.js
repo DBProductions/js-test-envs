c6c06745a37dc85baa7cc8bb8487744ebc60ec21
DBProductions <dennis@dbproductions.de>
2013-05-12 06:28:03 +0800

    update tests

--- qunit-sinon/tests/qunitsinonTest.js
+++ qunit-sinon/tests/qunitsinonTest.js
@@ -2,6 +2,23 @@ test("Using spies", function () {
     var spy = sinon.spy();
 	spy();
 	
-	ok(spy.called);
-	ok(spy.calledOnce); 
+	ok(spy.called, 'spy called');
+	ok(spy.calledOnce, 'spy called once');
+	equal(spy.callCount, 1, 'spy callCount is 1');
+
+	spy();
+	equal(spy.callCount, 2, 'spy callCount is 2');
+
+	spy();
+	notEqual(spy.callCount, 4, 'spy callCount is not 4');
+});
+
+test("Using stubs", function () {
+    var callback = sinon.stub();
+    callback.returns(42);
+    callback.withArgs(1).returns(1);
+
+    equal(callback(), 42, 'stub called without argument');
+    equal(callback(1), 1, 'stub called with argument');
+    equal(callback(10), 42, 'stub called with argument');
 });
--- qunit/tests/qunitTest.js
+++ qunit/tests/qunitTest.js
@@ -1,16 +1,22 @@
 module("module A");
 
 test("first test", function() {
-    ok( 1 == "1", "Passed!" );
+    ok(1 == "1", "Passed!");
+    equal(1, 1, "Passed!");
+    notEqual(1, 2, "Passed!");
 });
 
 test("second test", function() {
     ok( 1 == "1", "Passed!" );
+    equal(1, 1, "Passed");
+    notEqual(1, 2, "Passed!");
 });
 
 module("module B", {
+    uid: null,
     setup: function() {
         // prepare something for all following tests
+        this.uid = 5;
     },
     teardown: function() {
         // clean up after each test
@@ -19,9 +25,12 @@ module("module B", {
 
 test("first test", function() {
     ok( 1 == "1", "Passed!" );
+    equal(this.uid, 5, "Passed!");
+    notEqual(this.uid, 6, "Passed!");
 });
 
 test("second test", function() {
     ok( 1 == "1", "Passed!" );
-    ok( 1 == "1", "Passed!" );
+    equal(this.uid, 5, "Passed!");
+    notEqual(this.uid, 6, "Passed!");
 });
